89c3493b15aeb513f2f518576e6f3c7afd985977
elot <elliottgold96@gmail.com>
2020-12-30 18:07:36 -0500

    onyx block

--- src/main/java/com/elot/emeraldbreeze/core/init/BlockInit.java
+++ src/main/java/com/elot/emeraldbreeze/core/init/BlockInit.java
@@ -16,9 +16,8 @@ public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
             EmeraldBreeze.MOD_ID);
 
-    public static final RegistryObject<Block> ONYX_BLOCK = BLOCKS.register("onyx_block",
-            () -> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.BLACK)
-                    .hardnessAndResistance(15f, 30f)
-                    .harvestTool(ToolType.PICKAXE).harvestLevel(2)
-                    .sound(SoundType.METAL)));
+    public static final RegistryObject<Block> ONYX_BLOCK = BLOCKS.register("onyx_block", () -> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.BLACK).hardnessAndResistance(5.0f, 30.0f).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> SILVER_BLOCK = BLOCKS.register("silver_block", () -> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0f,30.0f).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.METAL)));
+
+
 }
