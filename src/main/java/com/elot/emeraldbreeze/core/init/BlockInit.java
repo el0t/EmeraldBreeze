dc8792aef431917b51061a81c7f7bb2cfb50b053
elot <elliottgold96@gmail.com>
2020-12-30 18:19:13 -0500

    ingots and blocks from ElotPack

--- src/main/java/com/elot/emeraldbreeze/core/init/BlockInit.java
+++ src/main/java/com/elot/emeraldbreeze/core/init/BlockInit.java
@@ -16,8 +16,17 @@ public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
             EmeraldBreeze.MOD_ID);
 
-    public static final RegistryObject<Block> ONYX_BLOCK = BLOCKS.register("onyx_block", () -> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.BLACK).hardnessAndResistance(5.0f, 30.0f).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.METAL)));
-    public static final RegistryObject<Block> SILVER_BLOCK = BLOCKS.register("silver_block", () -> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.STONE).hardnessAndResistance(5.0f,30.0f).harvestTool(ToolType.PICKAXE).harvestLevel(2).sound(SoundType.METAL)));
-
+    public static final RegistryObject<Block> ONYX_BLOCK = BLOCKS.register("onyx_block",
+            () -> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.BLACK)
+                    .hardnessAndResistance(5.0f, 30.0f).harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(2).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> SILVER_BLOCK = BLOCKS.register("silver_block",
+            () -> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.STONE)
+                    .hardnessAndResistance(5.0f,30.0f).harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(2).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> MITHRIL_BLOCK = BLOCKS.register("mithril_block",
+            ()-> new Block(AbstractBlock.Properties.create(Material.IRON, MaterialColor.BLUE)
+                    .hardnessAndResistance(12.0f,45.0f).harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(3).sound(SoundType.METAL)));
 
 }
