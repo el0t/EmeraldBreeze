83300eb8e9181caa87103f078c3c156e3c356466
elot <elliottgold96@gmail.com>
2021-01-02 09:22:09 -0500

    added ModItemTier and ModArmorMaterial enums

---
+++ src/main/java/com/elot/emeraldbreeze/items/ModArmorMaterial.java
@@ -0,0 +1,87 @@
+package com.elot.emeraldbreeze.items;
+
+import com.elot.emeraldbreeze.EmeraldBreeze;
+import com.elot.emeraldbreeze.core.init.ItemInit;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterial implements IArmorMaterial {
+    /* name, maxDamageFactor, damageReductionAmountArray, enchantability, soundEvent, toughness, knockbackResistance, repairMaterial
+   LEATHER  ("leather", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> { return Ingredient.fromItems(Items.LEATHER);}),
+   CHAIN    ("chainmail", 15, new int[]{1, 4, 5, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> { return Ingredient.fromItems(Items.IRON_INGOT);}),
+   IRON     ("iron", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> { return Ingredient.fromItems(Items.IRON_INGOT);}),
+   GOLD     ("gold", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, 0.0F, () -> { return Ingredient.fromItems(Items.GOLD_INGOT);}),
+   DIAMOND  ("diamond", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> { return Ingredient.fromItems(Items.DIAMOND);}),
+   TURTLE   ("turtle", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_TURTLE, 0.0F, 0.0F, () -> { return Ingredient.fromItems(Items.SCUTE);}),
+   NETHERITE("netherite", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> { return Ingredient.fromItems(Items.NETHERITE_INGOT);});*/
+
+    //Mod items
+
+    MITHRIL(EmeraldBreeze.MOD_ID+":mithril", 33, new int[]{3, 5, 7, 3}, 9, SoundEvents.ITEM_ARMOR_EQUIP_GOLD,
+            2.0F, 0.0F, () -> { return Ingredient.fromItems(ItemInit.MITHRIL_INGOT.get());
+    });
+
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    private ModArmorMaterial(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountsIn, int enchantabilityIn, SoundEvent equipSoundIn, float toughnessIn, float knockbackResistanceIn, Supplier<Ingredient> repairMaterialSupplier) {
+        this.name = nameIn;
+        this.maxDamageFactor = maxDamageFactorIn;
+        this.damageReductionAmountArray = damageReductionAmountsIn;
+        this.enchantability = enchantabilityIn;
+        this.soundEvent = equipSoundIn;
+        this.toughness = toughnessIn;
+        this.knockbackResistance = knockbackResistanceIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialSupplier);
+    }
+
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public SoundEvent getSoundEvent() {
+        return this.soundEvent;
+    }
+
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.getValue();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public String getName() {
+        return this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return knockbackResistance;
+    }
+}
---
+++ src/main/java/com/elot/emeraldbreeze/items/ModItemTier.java
@@ -0,0 +1,65 @@
+package com.elot.emeraldbreeze.items;
+
+import com.elot.emeraldbreeze.core.init.ItemInit;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+    /* harvestLevel, maxUses, efficiency, attackDamage, enchantability, repairMaterial
+        WOOD    (0, 59, 2.0F, 0.0F, 15, () -> {return Ingredient.fromTag(ItemTags.PLANKS);}),
+        STONE   (1, 131, 4.0F, 1.0F, 5, () -> {return Ingredient.fromTag(ItemTags.STONE_TOOL_MATERIALS);}),
+        IRON    (2, 250, 6.0F, 2.0F, 14, () -> {return Ingredient.fromItems(Items.IRON_INGOT);}),
+        DIAMOND (3, 1561, 8.0F, 3.0F, 10, () -> {return Ingredient.fromItems(Items.DIAMOND);}),
+        GOLD    (0, 32, 12.0F, 0.0F, 22, () -> {return Ingredient.fromItems(Items.GOLD_INGOT);}),
+      NETHERITE (4, 2031, 9.0F, 4.0F, 15, () -> {return Ingredient.fromItems(Items.NETHERITE_INGOT);});*/
+
+    //Mod items
+
+    MITHRIL(3, 750, 8.0F, 3.0F, 9, () -> {
+        return Ingredient.fromItems(ItemInit.MITHRIL_INGOT.get());
+    });
+
+    private ModItemTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn){
+        this.harvestLevel = harvestLevelIn;
+        this.maxUses = maxUsesIn;
+        this.efficiency = efficiencyIn;
+        this.attackDamage = attackDamageIn;
+        this.enchantability = enchantabilityIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+    }
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.getValue();
+    }
+}
