0c926ad913c44a8e626cdb9045169d3afd2dc7a1
elot <elliottgold96@gmail.com>
2021-01-09 16:27:13 -0500

    fixed lang and made popcorn fastToEat

--- src/main/java/com/elot/emeraldbreeze/items/ModFoods.java
+++ src/main/java/com/elot/emeraldbreeze/items/ModFoods.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Food;
 public class ModFoods {
 
     public static final Food GARLIC = (new Food.Builder().hunger(2).saturation(0.2F).build());
-    public static final Food POPCORN = (new Food.Builder().hunger(1).setAlwaysEdible().build());
+    public static final Food POPCORN = (new Food.Builder().hunger(1).setAlwaysEdible().fastToEat().build());
 
 
 
