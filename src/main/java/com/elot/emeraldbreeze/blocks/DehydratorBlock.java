ea5b593a827df45957ad1e7dae55fc86e5f19a25
elot <elliottgold96@gmail.com>
2021-01-15 16:34:35 -0500

    fixed issue with activating held item when opening Dehydrator GUI

--- src/main/java/com/elot/emeraldbreeze/blocks/DehydratorBlock.java
+++ src/main/java/com/elot/emeraldbreeze/blocks/DehydratorBlock.java
@@ -15,7 +15,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nullable;
