95a8ab6eb0d5ed423b3dd60284b444c34650957c
elot <elliottgold96@gmail.com>
2021-02-09 16:55:37 -0500

    fix grass....

--- src/main/java/com/elot/emeraldbreeze/core/init/ItemInit.java
+++ src/main/java/com/elot/emeraldbreeze/core/init/ItemInit.java
@@ -44,7 +44,7 @@ public class ItemInit {
     public static final RegistryObject<Item> SAGE = ITEMS.register("sage", () -> new Item(new Item.Properties().food(ModFoods.SAFE_HERBS).group(EmeraldBreeze.PLANTS)));
     public static final RegistryObject<Item> MINT = ITEMS.register("mint", () -> new Item(new Item.Properties().food(ModFoods.SAFE_HERBS).group(EmeraldBreeze.PLANTS)));
         //need source
-        //test chamo source
+    //TODO FIX GRASS
     public static final RegistryObject<Item> CHAMOMILE = ITEMS.register("chamomile", () -> new Item(new Item.Properties().food(ModFoods.SAFE_HERBS).group(EmeraldBreeze.PLANTS)));
 
     //TODO nettle crop
